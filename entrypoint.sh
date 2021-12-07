1d7ef24885a2dad8d0b6a6e89f74db2ae84e9561
Sahan Serasinghe <sahan.serasinghe@gmail.com>
2021-12-07 22:26:32 +1030

    Parameterize the entire script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,63 @@
 #!/bin/sh
 
+# Usage:
+# ./entrypoint.sh \
+#     --version 1.3.0 --package io.dapr.client --class DaprClientBuilder \
+#     --methods "withObjectSerializer withStateSerializer builder" \
+#     --keep-going=1
+
 set -u
 
+# Set default values
 Version="1.3.1"
 Package="io.dapr.client"
 Class="DaprClientBuilder"
-DaprClientBuilderMethods="withObjectSerializer withStateSerializer"
+Methods="withObjectSerializer withStateSerializer"
 KeepGoingCount="1"
 
-for Method in ${DaprClientBuilderMethods}; do
-  echo "Fuzzing $Method"
+POSITIONAL=()
+while [[ $# -gt 0 ]]; do
+  key="$1"
+  case $key in
+    -v|--version)
+      Version="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -p|--package)
+      Package="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -c|--class)
+      Class="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -m|--methods)
+      Methods="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -kc|--keep-going)
+      KeepGoingCount="$2"
+      shift # past argument
+      ;;
+    *)    # unknown option
+      POSITIONAL+=("$1") # save it in an array for later
+      shift # past argument
+      ;;
+  esac
+done
+
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+echo "Version   = ${Version}" 
+echo "Package   = ${Package}" 
+echo "Class     = ${Class}"
+
+for Method in ${Methods}; do
+  echo "Fuzzing Method = ${Method}"
   docker run --rm -v /"$(pwd)/fuzzing/$Method":/fuzzing cifuzz/jazzer-autofuzz \
    io.dapr:dapr-sdk:${Version} \
    ${Package}.${Class}::${Method} --keep_going=${KeepGoingCount}
